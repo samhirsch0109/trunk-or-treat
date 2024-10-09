c93787bc7719908d2cb89e0fcbdb0764f79bdb10
samhirsch0109 <sammyhirsch0109@gmail.com>
2024-10-09 11:55:35 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    // Toggle the visibility of the information div when clicking on #more-info
+    $('#more').on('click') {
+        $('information').toggle();
+    });
