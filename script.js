173d539429184303ec8501eb8234842ee08890a6
samhirsch0109 <144719425+samhirsch0109@users.noreply.github.com>
2024-10-11 12:26:16 +0000

    added pictures

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    // Toggle the visibility of the information div when clicking on #more-info
-    $('#more').on('click') {
-        $('information').toggle();
+    $('.toggle-button').on('click', function () {
+        $('.information').toggle();
     });
+});
