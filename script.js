2a0b296da1f63528c3c04ae5140ed7e6b56fd180
samhirsch0109 <144719425+samhirsch0109@users.noreply.github.com>
2024-10-14 20:05:44 +0000

    did most of layout

--- script.js
+++ script.js
@@ -4,4 +4,22 @@ $(document).ready(function () {
     });
 });
 
-$('')
+$('.trunk-image').on('mouseenter', function (event) {
+    $(event.currentTarget).next('.trunk-description').fadeIn(1000);
+});
+
+$('.trunk-image').on('mouseleave', function (event) {
+    $(event.currentTarget).next('.trunk-description').fadeOut(1000);
+});
+
+$('#event1-heading').on('click', function () {
+    $('#event1-details').slideToggle();
+});
+
+$('#event2-heading').on('click', function () {
+    $('#event2-details').slideToggle();
+});
+
+$('#event3-heading').on('click', function () {
+    $('#event3-details').slideToggle();
+});
